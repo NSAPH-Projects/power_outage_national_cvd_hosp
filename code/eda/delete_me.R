5339d332ed1bee6ecee9893c166dd2fadd443b74
heathermcb <heather.mcbrien@mail.utoronto.ca>
2024-10-11 17:48:31 +0000

    deleted old files

--- code/eda/delete_me.R
+++
@@ -1,6 +0,0 @@
-
-pacman::p_load(arrow, data.table, tidyverse, here)
-
-outages <- read_parquet(here('data', 'analytic_exposure_data_2018.parquet'))
-
-outages %>% ggplot() + geom_point(aes(x = day, y = n_hrs_out_0.01))
